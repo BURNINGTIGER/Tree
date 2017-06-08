5159423fcbdbaca78d94882db97516a3a127eaa9
BURNINGTIGER <Aleks203467@gmail.com>
2017-06-08 09:14:23 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -8,14 +8,6 @@ SCENARIO("matrix init", "[init]") {
 	Matrix matrix;
 	REQUIRE(matrix.rows() == 0);
 	REQUIRE(matrix.columns() == 0);
-}
-
-SCENARIO("params init", "[init with params]") {
-	int init = 5;
-	Matrix matrix(init, init);
-	REQUIRE(matrix.rows() == 5);
-	REQUIRE(matrix.columns() == 5);
-}
 
 
 
